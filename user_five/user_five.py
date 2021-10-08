a26d91cf35da65f8abe07360379f43f1d217a09b
expleo <Amit.hiremath@expleogroup.com>
2021-10-08 09:29:44 +0530

    User_five_times

---
+++ user_five/user_five.py
@@ -0,0 +1,9 @@
+Name=input("enter your Name: ")
+ManagerName=input("enter your Manager Name : ")
+Title=input("enter project title Name: ")
+
+for j in range(0,5):
+        print(Name)
+        print(ManagerName)
+        print(Title)
+        print(" ")
